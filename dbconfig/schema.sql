3d8af2ea38069f81035a25005133862463da109a
PauloCavalcanteDev <dev.paulocavalcante@gmail.com>
2024-03-11 21:04:07 -0300

    chore: configura api-fakes.

--- dbconfig/schema.sql
+++ dbconfig/schema.sql
@@ -1,21 +1,11 @@
 
-
-CREATE TABLE IF NOT EXISTS CLIENTES (
-
-	cliente_id bigint  PRIMARY KEY not null,
-    nome varchar(36),
-	conta_id bigint NOT NULL
-
-);
-CREATE INDEX IF NOT EXISTS cliente_id ON CLIENTES (cliente_id);
-
 CREATE TABLE IF NOT EXISTS CONTAS (
     conta_id bigint PRIMARY KEY,
     cliente_id bigint not null,
-    limite numeric(1000,2),
+    limite_diario numeric(1000,2),
     saldo  numeric(1000,2)
 );
-CREATE INDEX IF NOT EXISTS contas ON CONTAS (conta_id, cliente_id);
+-- CREATE INDEX IF NOT EXISTS contas ON CONTAS (conta_id, cliente_id);
 
 
 CREATE TABLE IF NOT EXISTS TRANSACOES (
@@ -24,6 +14,7 @@ CREATE TABLE IF NOT EXISTS TRANSACOES (
     cliente_id bigint,
     tipo varchar(20),
     valor numeric(1000,2),
+    status_bacen varchar(15),
     data_hora TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
@@ -32,9 +23,20 @@ CREATE INDEX IF NOT EXISTS transacao ON TRANSACOES (conta_id, cliente_id, id_tra
 --
 --
 
---INSERT INTO saldos (id, cliente_id, valor, limite)
---        VALUES (1, 1, 0, 1000 * 100),
---               (2, 2, 0, 800 * 100),
---               (3, 3, 0, 10000 * 100),
---               (4, 4, 0, 100000 * 100),
---               (5, 5, 0, 5000 * 100);
+INSERT INTO CONTAS (conta_id, cliente_id , limite_diario, saldo)
+       VALUES (10, 10,1000.00 , 2000.00),
+              (20, 11,1000.00 , 2000.00),
+              (30, 12,1000.00 , 2000.00),
+              (40, 13,1000.00 , 2100.00),
+              (50, 14,1000.00 , 3000.00),
+              (60, 15,1000.00 , 1200.00),
+              (70, 16,1000.00 , 4000.00),
+              (80, 17,1000.00 , 3000.00),
+              (90, 18,1000.00 , 3000.00),
+              (100,19,1000.00 , 3000.00),
+              (85, 20,1000.00 , 3000.00);
+         
+            --   (2, 2, 0, 800 * 100),
+            --   (3, 3, 0, 10000 * 100),
+            --   (4, 4, 0, 100000 * 100),
+            --   (5, 5, 0, 5000 * 100);
